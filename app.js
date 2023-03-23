cdd3b47e6cbd2c33509159aa9ca18279642b15a3
vishwanath tanmai <122545477+VishwanathTanmai@users.noreply.github.com>
2023-03-23 21:04:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ form.addEventListener("submit", async (e) => {
       prompt: message,
       model: "text-davinci-003",
       temperature: 0,
-      max_tokens: 2000,
+      max_tokens: 1000,
       top_p: 1,
       frequency_penalty: 0.0,
       presence_penalty: 0.0,
